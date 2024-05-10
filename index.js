a2225694a3fe9074c86e7d7bbe96763345404682
saiful797 <saifulislamsaif797@gmail.com>
2024-05-10 22:39:10 +0600

    Get data from mongodb database seerver and data shows successfully

--- index.js
+++ index.js
@@ -27,6 +27,16 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+    const roomsCollections = client.db('hotelRoomsDB').collection('rooms');
+
+    app.get('/rooms', async(req, res) => {
+        const result = await roomsCollections.find().toArray();
+        res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
