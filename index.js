fac97e85c6fa5e81337358b8a7b7eca7804ec3cb
saiful797 <saifulislamsaif797@gmail.com>
2024-05-11 15:24:57 +0600

    Updated server site and get data from mongodb server

--- index.js
+++ index.js
@@ -41,7 +41,6 @@ async function run() {
         // console.log(result)
     })
 
-
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
